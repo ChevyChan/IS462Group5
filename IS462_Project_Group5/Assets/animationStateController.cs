3c7f3db2d8a99b9e49f645fc9fc079fcba9f1703
Alvin Owyong <alvinowyong@gmail.com>
2023-03-01 04:05:55 +0800

    feat: splash screen, screen and script changes

---
+++ IS462_Project_Group5/Assets/animationStateController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    private double start;
+    Rigidbody m_Rigidbody;
+    public GameObject target;
+    public AudioSource audioSource;
+    public AudioClip crashSound;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = crashSound;
+        audioSource.Play();
+        start = Time.realtimeSinceStartup;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // transform.position = moveSpeed * Time.deltaTime * transform.forward;
+
+
+        float timeNow = Time.realtimeSinceStartup;
+        if (timeNow > 120)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", true);
+        } else
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isWalking", true);
+        }
+        // var lookPos = target.transform.position - transform.position;
+        // var rotation = Quaternion.LookRotation(lookPos);
+        // transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2);
+        //transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 10F * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //if (other.gameObject.CompareTag("Player"))
+        //{
+        //    if (currentPoints <= 30)
+        //    {
+        //        SceneManager.LoadScene("GameEndScene");
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Car ran into obstacle");
+        //        audioSource.clip = crashSound;
+        //        audioSource.Play();
+        //        currentPoints -= 30;
+        //        points.text = currentPoints.ToString();
+        //    }
+        //}
+        //else if (other.gameObject.CompareTag("Goal"))
+        //{
+        //    Debug.Log("Car reached destination");
+        //    audioSource.clip = clapSound;
+        //    audioSource.Play();
+        //}
+
+    }
+}
